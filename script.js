13754c724b912c3b37093b0540f5a3a273ed75fb
JuneInbox <juneinbox@proton.me>
2025-01-31 21:45:43 +0000

    Fixed

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
-let paperBtn = document.querySelector('#paper')
-let rockBtn = document.querySelector('#rock')
-let scissorsBtn = document.querySelector('#scissors')
-let restart = document.querySelector('#restart')
-let playerScore = 0
-let computerScore = 0
-let gameOver = false;
+let paperBtn = document.querySelector('#paper');
+let rockBtn = document.querySelector('#rock');
+let scissorsBtn = document.querySelector('#scissors');
+let restart = document.querySelector('#restart');
+let playerScore = 0;
+let computerScore = 0;
+let gameOver = false;  
 
 
-// Plays a round everytime the player clicks one of the buttons, set to run the function with the paramater in HTML!!!
 function playRound(playerChoice) {
-
-    if (gameOver) return;
+    if (gameOver) return; 
 
     let computerChoice = getComputerChoice();
 
@@ -18,61 +16,61 @@ function playRound(playerChoice) {
         playerChoice == 'paper' && computerChoice == 'rock' ||
         playerChoice == 'scissors' && computerChoice == 'paper') {
         playerScore += 1;
-        document.getElementById("playerscore").innerHTML = "Player score is" + " " + playerScore;
-        document.getElementById("computerscore").innerHTML = "Computer score is" + " " + computerScore;
-        return playerScore;
+        document.getElementById("playerscore").innerHTML = "Player score is " + playerScore;
+        document.getElementById("computerscore").innerHTML = "Computer score is " + computerScore;
     } else if (playerChoice == 'rock' && computerChoice == 'paper' ||
         playerChoice == 'paper' && computerChoice == 'scissors' ||
         playerChoice == 'scissors' && computerChoice == 'rock') {
-        computerScore += 1
-        document.getElementById("computerscore").innerHTML = "Computer score is" + " " + computerScore;
-        document.getElementById("playerscore").innerHTML = "Player score is" + " " + playerScore;
-        return computerScore;
+        computerScore += 1;
+        document.getElementById("computerscore").innerHTML = "Computer score is " + computerScore;
+        document.getElementById("playerscore").innerHTML = "Player score is " + playerScore;
     } else if (playerChoice === computerChoice) {
-       document.getElementById("computerscore").innerHTML = "It's a tie!"
-       document.getElementById("playerscore").innerHTML = "It's a tie!"
-    } 
-    // Game won function!!!!!11
+        document.getElementById("computerscore").innerHTML = "It's a tie!";
+        document.getElementById("playerscore").innerHTML = "It's a tie!";
+    }
+
+  
     if (playerScore == 5) {
         alert("You win!");
-        gameOver = true;
-        disableButtons();
+        gameOver = true;  
+        disableButtons(); 
     } else if (computerScore == 5) {
         alert("You lose!");
-        gameOver = true;
-        disableButtons();
+        gameOver = true;  
+        disableButtons(); 
     }
-    
 };
 
-// Function to get theeee computers choice, chooses a random number between 0-2!!
-function getComputerChoice() {
-    let computerNum = Math.floor(Math.random() * 3);
-
-    if (computerNum === 0) {
-        computerNum = 'rock'
-    } else if (computerNum === 1) {
-        computerNum = 'paper'
-    } else {
-        computerNum = 'scissors'
-    };
-    return computerNum;
-};
 
 function disableButtons() {
     rockBtn.disabled = true;
     paperBtn.disabled = true;
     scissorsBtn.disabled = true;
+};
+
+
+function getComputerChoice() {
+    let computerNum = Math.floor(Math.random() * 3);
+
+    if (computerNum === 0) {
+        return 'rock';
+    } else if (computerNum === 1) {
+        return 'paper';
+    } else {
+        return 'scissors';
+    }
 }
 
-// Restart button to restart da game upon win, disables extra buttons to try n prevent game breaking
+
 restart.addEventListener('click', () => {
     playerScore = 0;
     computerScore = 0;
+    gameOver = false;  
+
     document.getElementById("playerscore").innerHTML = "Player score is 0";
     document.getElementById("computerscore").innerHTML = "Computer score is 0";
+
     rockBtn.disabled = false;
-    scissorsBtn.disabled = false;
     paperBtn.disabled = false;
-    gameOver = false;
+    scissorsBtn.disabled = false;
 });
